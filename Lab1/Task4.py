4bf781a1dfa53a2a471754b93ae72fd952cb1f81
danil <danilskor2000@mail.ru>
2022-10-26 15:43:05 +0700

    Task4

---
+++ Lab1/Task4.py
@@ -0,0 +1,40 @@
+import random
+import random as ran
+
+
+def task1_sub_task1():
+    a = float(input())
+    b = float(input())
+    if a > b:
+        print(a)
+    elif a < b:
+        print(b)
+    else:
+        print('a = b')
+
+
+def task1_sub_task2():
+    a = float(input())
+    if a > 0:
+        print('введённое число больше нуля')
+    elif a < 0:
+        print('введённое число меньше нуля')
+    else:
+        print('введённое число равно нулю')
+
+
+def task1_sub_task3():
+    valueList = [[0] * 5 for i in range(5)]
+    for i in range(5):
+        for j in range(5):
+            valueList[i][j] = int(random.uniform(-10, 10))
+    print(valueList)
+    summa = 0
+    for width in valueList:
+        for ind in width:
+            if ind > 0:
+                summa = summa + ind
+    print(summa)
+
+
+task1_sub_task3()
