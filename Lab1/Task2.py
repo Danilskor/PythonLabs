13167e69f2b251ab41c453f688cfd0ea9816f8b0
danil <danilskor2000@mail.ru>
2022-11-03 09:12:14 +0700

    Rename subtasks

--- Lab1/Task2.py
+++ Lab1/Task2.py
@@ -3,14 +3,14 @@ from math import *
 
 # Вариант 1
 
-def a():
+def task2_a():
     m = -14 / 15
     n = tan(pi / 8)
     expression = 3 * m ** 2 + pow(2 * n ** 2, 1 / 3 / m)
     print('m =', m, 'n =', n, 'Результат =', expression)
 
 
-def b():
+def task2_b():
     print('Введите m')
     m = float(input())
     print('Введите n')
